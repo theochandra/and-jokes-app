55bd33798a62586575cdcf63fa257937acbec357
theodorous.yuvens <theodorus_beatayuvens@aeon.co.id>
2021-02-22 16:44:37 +0700

    add joke mapper

---
+++ data/src/main/java/com/android/data/mapper/JokeMapper.kt
@@ -0,0 +1,29 @@
+package com.android.data.mapper
+
+import com.android.data.response.JokeListResponse
+import com.android.data.response.JokeResponse
+import com.android.domain.model.Joke
+import com.android.domain.model.JokeList
+
+class JokeMapper {
+
+    fun map(response: JokeResponse): Joke {
+        return Joke(
+            categories = response.categories,
+            createdAt = response.createdAt,
+            iconUrl = response.iconUrl,
+            id = response.id,
+            updatedAt = response.updatedAt,
+            url = response.url,
+            value = response.value
+        )
+    }
+
+    fun map(response: JokeListResponse): JokeList {
+        return JokeList(
+            total = response.total,
+            result = response.result.map { map(it) }
+        )
+    }
+
+}
