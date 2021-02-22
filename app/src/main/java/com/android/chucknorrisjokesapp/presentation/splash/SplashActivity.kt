8c6ee1c1fed5673307e6ab1d07aa315579fde02a
theodorous.yuvens <theodorus_beatayuvens@aeon.co.id>
2021-02-22 18:11:55 +0700

    get joke categories in splash screen

--- app/src/main/java/com/android/chucknorrisjokesapp/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/android/chucknorrisjokesapp/presentation/splash/SplashActivity.kt
@@ -1,12 +1,15 @@
 package com.android.chucknorrisjokesapp.presentation.splash
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.android.chucknorrisjokesapp.R
 import com.android.chucknorrisjokesapp.base.BaseActivity
 import com.android.chucknorrisjokesapp.databinding.ActivitySplashBinding
 import com.android.chucknorrisjokesapp.di.Injector
+import com.android.domain.Result
 import javax.inject.Inject
 
 class SplashActivity : BaseActivity() {
@@ -30,5 +33,42 @@ class SplashActivity : BaseActivity() {
 
         binding.viewModel = viewModel
 
+        observeStateData()
     }
+
+    private fun observeStateData() {
+        viewModel.stateData.observe(this, { result ->
+            when(result) {
+                Result.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is Result.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    val categories = result.data
+                    for (category in categories) {
+                        Log.i("SplashActivity", "Category ::: $category")
+                    }
+                }
+                is Result.Error -> {
+                    binding.progressBar.visibility = View.GONE
+                    showAlertMessage(
+                        getString(R.string.label_dialog_title_alert),
+                        result.errorMessage,
+                        getString(R.string.label_button_oke)
+                    )
+                }
+                is Result.Exception -> {
+                    binding.progressBar.visibility = View.GONE
+                    result.exception.message?.let { errorMessage ->
+                        showAlertMessage(
+                            getString(R.string.label_dialog_title_alert),
+                            errorMessage,
+                            getString(R.string.label_button_oke)
+                        )
+                    }
+                }
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/android/chucknorrisjokesapp/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/android/chucknorrisjokesapp/presentation/splash/SplashViewModel.kt
@@ -1,10 +1,32 @@
 package com.android.chucknorrisjokesapp.presentation.splash
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.domain.Result
 import com.android.domain.usecase.GetJokeCategoriesUseCase
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class SplashViewModel @Inject constructor(
     private val useCase: GetJokeCategoriesUseCase
 ) : ViewModel() {
+
+    private val _stateData = MutableLiveData<Result<List<String>>>()
+    val stateData: LiveData<Result<List<String>>>
+        get() = _stateData
+
+    init {
+        getJokeCategories()
+    }
+
+    private fun getJokeCategories() {
+        viewModelScope.launch {
+            _stateData.value = Result.Loading
+            val result = useCase.execute()
+            _stateData.value = result
+        }
+    }
+
 }
